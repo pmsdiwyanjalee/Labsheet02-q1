25ed078964248767338949ad124694ac15ae60bc
User <User@DESKTOP-2OOA2TK>
2024-02-07 13:36:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet02_q1
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Title = "The Great Gatsby";
+            myBook.Author = "F. Scott Fitzgerald";
+
+            Console.WriteLine("Title:" + myBook.Title);
+            Console.WriteLine("Author:" + myBook.Author);
+            Console.ReadLine();
+        }
+    }
+}
